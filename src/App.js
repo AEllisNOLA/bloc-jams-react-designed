004e4bb329197bc1006146862dd193b8017320da
Anthony <aellisnola@gmail.com>
2019-03-12 17:16:00 -0500

    Created/Styled Landing and Nav

--- src/App.js
+++ src/App.js
@@ -9,21 +9,40 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="ui container">
-          <header>
-            <nav>
-              <Link to="/">Landing</Link>
-              <Link to="/library">Library</Link>
-              <Link to="/album">Album</Link>
-            </nav>
-            <h1>Bloc Jams</h1>
-          </header>
-          <main>
-            <Route exact path="/" component={Landing} />
-            <Route path="/library" component={Library} />
-            <Route path="/album" component={Album} />
-          </main>
+        <div className="ui large top fixed hidden borderless inverted menu">
+          <nav className="ui container">
+            <Link className="item" to="/">
+              <img
+                id="logo"
+                className="logo"
+                src={require("./assets/img/bloc_jams_logo.png")}
+                alt="Bloc Jams logo"
+              />
+            </Link>
+            <div className="menu right">
+              <Link className="item" to="/library">
+              <p className="nav-item">Library</p>
+              </Link>
+              <Link className="item" to="/album">
+                <p className="nav-item">Album</p>
+              </Link>
+            </div>
+          </nav>
         </div>
+
+       
+        <header>
+          <nav>
+            <Link to="/">Landing</Link>
+            <Link to="/library">Library</Link>
+            <Link to="/album">Album</Link>
+          </nav>
+        </header>
+        <main>
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
+          <Route path="/album" component={Album} />
+        </main>
       </div>
     );
   }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,36 @@
 import React from "react";
 
 const Landing = () => (
-  <div className="ui container">
-    <section className="landing">Landing page will go here.</section>
+  <div id="landing-image">
+    <div id="landing-overlay">
+      <section className="landing ui container">
+        <h1 className="hero-title">Turn the music up!</h1>
+        <section className="ui stackable three column grid">
+          <div className="column point">
+            <h2 className="point-title">Choose your music</h2>
+            <h3 className="point-description">
+              The world is full of music; why should you have to listen to music
+              that someone else chose?
+            </h3>
+          </div>
+
+          <div className="column point">
+            <h2 className="point-title">Unlimited, streaming, ad-free</h2>
+            <h3 className="point-description">
+              No arbitrary limits. No distractions.
+            </h3>
+          </div>
+
+          <div className="column point">
+            <h2 className="point-title">Mobile-enabled</h2>
+            <h3 className="point-description">
+              Listen to your music on-the-go. This streaming service is
+              available on all mobile platforms.
+            </h3>
+          </div>
+        </section>
+      </section>
+    </div>
   </div>
 );
 
