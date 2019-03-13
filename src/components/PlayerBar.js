f8630960d4b39ad06358dabfb33a789821d6b172
Anthony <aellisnola@gmail.com>
2019-03-13 14:28:35 -0500

    Time bar updating. Can click on progress bar to change time

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -10,8 +10,8 @@ class PlayerBar extends Component {
             <button id="previous" onClick={this.props.handlePrevClick}>
               <i className="backward icon" />
             </button>
-            
-            <button id="play-pause" onClick={this.props.handleSongClick} >
+
+            <button id="play-pause" onClick={this.props.handleSongClick}>
               <span>
                 <i
                   className={this.props.isPlaying ? "pause icon" : "play icon"}
@@ -25,11 +25,19 @@ class PlayerBar extends Component {
           </section>
 
           <section id="time-control">
-            <div className="current-time">-:--</div>
+            <div className="current-time">{this.props.currentTime}</div>
             <div className="ui slider range">
-              <input type="range" className="seek-bar" value="0" />
+              <input
+                type="range"
+                className="seek-bar"
+                value={(this.props.currentTime / this.props.duration) || 0}
+                max="1"
+                min="0"
+                step="0.01"
+                onChange={this.props.handleTimeChange}
+              />
             </div>
-            <div className="total-time">-:--</div>
+            <div className="total-time">{this.props.duration}</div>
           </section>
 
           <section id="volume-control">
